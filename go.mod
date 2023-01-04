6ed1cef742c3250d1cf1367c8c300c8e4d788db5
Garrett Jones <garrettcj2010@hotmail.com>
2023-01-04 00:10:53 -0600

    added initial refactorings still need to add hooks to UI

--- go.mod
+++ go.mod
@@ -4,22 +4,45 @@ go 1.18
 
 require (
 	github.com/charmbracelet/bubbles v0.13.0
-	github.com/charmbracelet/bubbletea v0.22.0
+	github.com/charmbracelet/bubbletea v0.23.1
 	github.com/charmbracelet/lipgloss v0.5.0
 )
 
 require (
+	cloud.google.com/go v0.105.0 // indirect
+	cloud.google.com/go/compute v1.13.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.2 // indirect
+	cloud.google.com/go/iam v0.8.0 // indirect
+	cloud.google.com/go/storage v1.28.1 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
 	github.com/containerd/console v1.0.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
-	github.com/muesli/cancelreader v0.2.1 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
-	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
+	github.com/muesli/termenv v0.13.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sahilm/fuzzy v0.1.0 // indirect
-	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.105.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20221206210731-b1a01be3a5f6 // indirect
+	google.golang.org/grpc v1.51.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
 
+	storage "cloud.google.com/go/storage"
 	"github.com/lonestar137/gcpfm/src/lib"
 	"github.com/lonestar137/gcpfm/src/ui"
+	"google.golang.org/api/iterator"
 )
 
 // Goal: make GCP ranger like filemanager, objective is to make GCP faster to navigate and use.
@@ -17,7 +22,9 @@ func ShowAvailBuckets() string {
 	//availBucketsOutput, err := exec.Command(cmd).Output()
 
 	//availBucketsOutput, err := exec.Command("gsutil", "ls", "-R", "\"gs://cill-raccoon-gj/\"").Output()
-	availBucketsOutput, err := exec.Command("gsutil", "ls").Output()
+	// availBucketsOutput, err := exec.Command("gsutil", "ls").Output()
+	// availBucketsOutput, err := exec.Command("cmd.exe", "/c", "ls").Output()
+	availBucketsOutput, err := exec.Command("ls", "-R", "-d", "/usr").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,29 +33,80 @@ func ShowAvailBuckets() string {
 	return ui.Home("Available buckets", parsedBucketsOutput)
 }
 
+type Directory struct {
+	title, desc, basePath string
+	platform              Platform
+}
+
+// enum type creation w/ companion pieces
+type Platform int
+
+const (
+	GCP Platform = iota // iota keyword instantiates all the contants w/ 0 to N values automatically, i.e. 0,1,2.  This is a unique integer, it prevents conflicts with other constants from other types .
+	WINDOWS
+	UNIX
+)
+
+// companion function to Platform type, now if we try to print our platform, it will use this function to print out the result instead of printing the integer.
+func (s Platform) String() string {
+	switch s {
+	case GCP:
+		return "GCP"
+	case WINDOWS:
+		return "WINDOWS"
+	case UNIX:
+		return "UNIX"
+	default:
+		return "Unknown"
+	}
+}
+
+func walkDirectory(baseDir Directory) {
+	// List the files and directories in the basDir
+
+	switch baseDir.platform {
+	case WINDOWS, UNIX:
+		filepath.WalkDir(baseDir.basePath, func(path string, info os.DirEntry, err error) error {
+			// logic for what to do w/ each detected dir
+			if err != nil {
+				return err
+			}
+			fmt.Println(path)
+			return nil
+		})
+	case GCP:
+		// Create a client for interacting with the Cloud Storage API
+		client, err := storage.NewClient(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// List the objects in the bucket
+		it := client.Bucket("my-bucket").Objects(context.Background(), nil)
+		for {
+			obj, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Print the object's path
+			fmt.Println(filepath.Join("gs://my-bucket", obj.Name))
+		}
+
+	default:
+		fmt.Println("Invalid platform.")
+	}
+
+}
+
 func main() {
 
-	// TODO Asap2, assign this to a value, and pass it to the exec command for the bucket.
-	var bucketName string = ShowAvailBuckets()
+	// List the files and directories in the current directory
 
-	// TODO put this on a concurrent block, that writes to the log file.
-	bucketFolderData, err := exec.Command("gsutil", "ls", "-R", "\""+bucketName+"\"").Output()
-	if err != nil {
-		fmt.Println("command failed to run.")
-		log.Fatal(err)
-	}
-	var parsedBucketFolderData []string = lib.ParseCmdOutput(bucketFolderData)
+	var root Directory = Directory{title: "Test", basePath: ".", desc: "just a test dir", platform: UNIX}
+	walkDirectory(root)
 
-	// TODO get the data from gsutil
-	//var bucketpath string = ""
-	//var cmd = "gsutil ls -R \"" + bucketpath + "\""
-	//exec.Command(cmd)
-
-	// TODO bubbletea loading icon while waiting for gsutil to return list.
-
-	if bucketName != "" {
-		if result := ui.Home(bucketName, parsedBucketFolderData); result != "" {
-			fmt.Println(result)
-		}
-	}
 }
